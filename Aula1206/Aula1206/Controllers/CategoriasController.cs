acf8a789190916d814647f874eec7ad85baeca65
1617867 <1617867@045LAB04035>
2017-06-12 10:50:26 -0300

    Formulário de cadastro

---
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -0,0 +1,47 @@
+﻿using Aula1206.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1206.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            //List<string> categorias = new List<string>();
+            /*categorias.Add("Carros");
+            categorias.Add("Motos");
+            categorias.Add("Barcos");
+            categorias.Add("Aviões");
+            categorias.Add("Caminhões");*/
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Carros", Descricao = "Super carros de coleção" });
+            categorias.Add(new Categoria() { Nome = "Motos", Descricao = "As moto mais velozes" });
+            categorias.Add(new Categoria() { Nome = "Barcos", Descricao = "Fragatas" });
+            categorias.Add(new Categoria() { Nome = "Aviões", Descricao = "Voadores" });
+            categorias.Add(new Categoria() { Nome = "Caminhões", Descricao = "Carga pesada" });
+            categorias.Add(new Categoria() { Nome = "Ônibus", Descricao ="Transporte"});
+
+            ViewBag.ListaCategorias = categorias;
+            return View(categorias);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            return View(categoria);
+        }
+    }
+}
---
+++ Aula1206/Aula1206/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula1206.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        [Required(ErrorMessage="*")]
+        public string Nome { get; set; }
+
+        [Display(Name ="Descrição"), DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
